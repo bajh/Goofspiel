eda95846ad944ce5c5c1e5e8dc85837c890f0876
Brandon Hopkins <brhpkns@gmail.com>
2014-08-30 14:11:57 -0400

    Card hand transform

--- public/page-behavior.js
+++ public/page-behavior.js
@@ -1,1 +1,34 @@
-page-behavior.js
+$(function(){
+  $('#player-hand').on('mouseenter', '.card', function(){
+    var cardInFocus = $(this).data("card-val");
+    var $cardsInHand = $('#player-hand').find('.card');
+    var rotationMultiplier = findIndexofCard(cardInFocus, $cardsInHand);
+    $cardsInHand.each(function(i){
+      var rotationMultiplier2 = rotationMultiplier;
+      console.log(rotationMultiplier2);
+      $(this).animate({borderSpacing: -90},
+        {step: function(now){
+          $(this).css('-webkit-transform','rotate('+rotationMultiplier2*(now/33)+'deg)'); 
+          $(this).css('-moz-transform','rotate('+rotationMultiplier2*(now/33)+'deg)');
+          $(this).css('transform','rotate('+rotationMultiplier2*(now/33)+'deg)');
+          },
+          duration:'slow'
+        },
+        'linear'
+      );
+      rotationMultiplier = rotationMultiplier2 - 1;
+    });
+  });
+
+  function findIndexofCard(card, hand){
+    var index;
+    hand.each(function(i){
+      if ($(this).data("card-val") == card) {
+        index = i;
+        return null;
+      }
+    });
+    return index;
+  }
+
+})
